7e96ec756580a98d9c4a5f486956d88aa27eb66f
assermoussa <asser.y.moussa@gmail.com>
2024-01-28 20:27:15 -0600

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+ // console.log (record[result]);
+  let i;
+function win(element, index, array){
+    if (record[result] === "W") {
+        i = record.year;
+        return true;
+      }
+}
+
+function superbowlWin(record){
+    const found = record.find((object) => object.result === "W" );
+    
+    /*if(found == true){
+        return found.year;
+    }
+    else{
+        return found;
+    }*/
+
+
+    if (found === undefined){
+        return found;
+    }
+    else {
+        return found.year;
+    }
+    
+}
